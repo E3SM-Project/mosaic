9e9520626ea54cb267a37903fb4043b8b39e0561
Andrew Nolan <anolan@lanl.gov>
2024-07-13 14:03:33 -0600

    Add skeleton of documentation.

---
+++ docs/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'mosaic'
+copyright = '2024, E3SM Development Team'
+author = 'E3SM Development Team'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+autosummary_generate = ['developers_guide/api.md']
+
+templates_path = ['_templates']
+
+exclude_patterns = ["_build", ".DS_Store"]
+
+# -- MyST settings -----------------------------------------------------------
+# copided from mache: https://github.com/E3SM-Project/mache
+myst_enable_extensions = [
+    'colon_fence',
+    'deflist',
+    'dollarmath'
+]
+myst_number_code_blocks = ["typescript"]
+myst_heading_anchors = 2
+myst_footnote_transition = True
+myst_dmath_double_inline = True
+myst_enable_checkboxes = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/E3SM-Project/mosaic",
+    "use_repository_button": True,
+}
+
+html_static_path = ['_static']
---
+++ docs/user_guide/quick_start.md
@@ -0,0 +1,31 @@
+# Quick Start
+
+
+## User Installation
+
+Coming soon....
+```
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
+conda install -y mosaic
+```
+
+## Developer Installation
+
+Assuming you have a working `conda` installation, you can install the latest development version of `mosaic` by running: 
+```
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda create -y -n mosaic-dev --file dev-environment.txt
+conda activate mosaic-dev
+python -m pip install -e .
+```
+
+If you have an existing `conda` environment you'd like install the development version of `mosaic` in, you can run: 
+```
+conda install --file dev-environment.txt
+
+python -m pip install -e .
+```
+
